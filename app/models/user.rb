4657077c2342968fe36403afb793f77bf4127d25
jkups <o.johnkupoluyi@gmail.com>
2021-01-08 23:29:52 +1100

    updated user model and addes migrations for user secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  has_secure_password #this line replaces line 6
   # validates :password_digest, confirmation: true
+
 
   has_many :reviews
   has_and_belongs_to_many :reservations
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_093054) do
+ActiveRecord::Schema.define(version: 2021_01_08_122620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 2021_01_08_093054) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
