ab2789c82cebf2329b4ad4b20e12f65868c3e700
david-Toledo <davidtoledo87@hotmail.com>
2021-01-11 09:59:57 +1100

    login added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get '/' => 'reservations#index'
+  
+  # get '/' => 'reservations#index'
 
   resources :reviews
   resources :reservations
   resources :users
+
+  root to: 'pages#home'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 
   # root 'hompage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
