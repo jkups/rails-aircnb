89d1dca21606facab59385de7b80c3d952ef6d85
david-Toledo <davidtoledo87@hotmail.com>
2021-01-13 18:16:03 +1100

    added return to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   def check_if_admin_logged_in
     unless @current_user.present? && @current_user.admin
       flash[:error] = 'Your account cannot perform that action.'
-      redirect_to 'http://localhost:3001/#/'
+      redirect_to 'http://localhost:3001/#/' and return
     end
   end
 
