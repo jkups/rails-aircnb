c01c72b9f6c870710287e1b9834821a577e7c4c5
Kyle Swaffield <kyle@swaff.id.au>
2021-01-13 12:14:57 +1100

    sync-change

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,7 @@
 class Review < ApplicationRecord
 
-<<<<<<< HEAD
   belongs_to :reservation, optional: true
   belongs_to :user, optional: true
   belongs_to :property, optional: true
-=======
-belongs_to :reservation, optional: true
-belongs_to :user, optional: true
-belongs_to :property, optional: true
->>>>>>> 64feda66e6268ca1d22fe4d84e99673b566ba863
 
 end
