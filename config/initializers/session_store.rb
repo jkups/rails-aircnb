2d1a22dd6bc09d4ff05679f0b3c8f6402b2b7393
Stacey <73633660+staceyrea@users.noreply.github.com>
2021-01-14 11:09:32 +1100

    pulling amended admin, installed chartkick and chartjs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_air-cnb', domain: 'https://air-bnb-replication.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store, key: '_air-cnb', domain: 'air-bnb-replication.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_air-cnb'
 end
