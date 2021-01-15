4e02b1100879733c4bc579f68420efc55900213c
jkups <o.johnkupoluyi@gmail.com>
2021-01-15 14:31:33 +1100

    update to session store and schema

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_air-cnb', domain: 'aircnb.herokuapp.com', same_site: :none, secure: :true
+  Rails.application.config.session_store :cookie_store, key: '_air-cnb', domain: 'air-bnb-replication.herokuapp.com', same_site: :none, secure: :true
 else
   Rails.application.config.session_store :cookie_store, key: '_air-cnb'
 end
