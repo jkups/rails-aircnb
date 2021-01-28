243b0bcf6a4ac677b1a6b6a1c5794135be94cd8c
jkups <o.johnkupoluyi@gmail.com>
2021-01-29 03:02:58 +1100

    updated authentication flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < ActionController::Base
 
   def check_if_admin_logged_in
     unless @current_user.present? && @current_user.admin
-      session[:user_id] = nil
       flash[:error] = 'Your account cannot perform that action.'
       redirect_to login_path
     end
